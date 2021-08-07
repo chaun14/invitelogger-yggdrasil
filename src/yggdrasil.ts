fe8dd90ac7b611fa4a59fa6234872d0d1f31ed6f
chaun14 <chaun1403@gmail.com>
2021-08-07 12:44:10 +0200

    Setup ws server

--- src/yggdrasil.ts
+++ src/yggdrasil.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { Server } from "ws";
+import config from "../config.json";
+export class YggdrasilClient extends Server {
+  public constructor(port: number) {
+    super({ port: port });
+  }
+}
+
+let client = new YggdrasilClient(config.port);
+
+client.on("listening", (data: any) => {
+  console.log("socket ready on port " + client.options.port);
+});
